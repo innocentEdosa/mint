c7b979a2679f1de32ee910333e698a9927b30413
innocentEdosa <ilegbinijieinnocentcareer@gmail.com>
2020-03-29 20:47:55 +0100

    [100] setup webpack config
    -split config into different files accounting for dev and prod
    - install the needed loaders from npm

---
+++ webpack.common.js
@@ -0,0 +1,64 @@
+const PATH = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: PATH.resolve(__dirname, 'dist'),
+    filename: '[name]-bundle.js',
+    publicPath: '/',
+  },
+  resolve: {
+    alias: {
+    },
+    extensions: ['.js', '.jsx', '.css'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react'],
+            plugins: ['@babel/plugin-transform-runtime', '@babel/plugin-proposal-class-properties'],
+          },
+        },
+      },
+      {
+        test: /\.(scss|css)$/,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+      {
+        test: /\.(png|jpe?g|gif)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {},
+          },
+        ],
+      },
+      {
+        test: /\.(png|jpg|gif|jpeg|svg|woff|woff2|ttf|otf|eot)$/i,
+        use: [
+          '@svgr/webpack',
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 10000,
+            },
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Phoenix',
+      template: './src/index.html',
+    }),
+    new CleanWebpackPlugin(),
+  ],
+};
