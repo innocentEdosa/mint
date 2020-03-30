19b9496458cec9382a2d9cccfb036a8d7baab13e
innocentEdosa <ilegbinijieinnocentcareer@gmail.com>
2020-03-30 19:49:15 +0100

    [110] create Table list

--- src/components/Payment/PaymentComponent.js
+++ src/components/Payment/PaymentComponent.js
@@ -5,6 +5,12 @@ import TableTopBar from 'components/Table/TableTopBar';
 import TableHead from 'components/Table/TableHead';
 import Paper from '@material-ui/core/Paper';
 import TableBottomBar from 'components/Table/TableBottomBar';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableRow from '@material-ui/core/TableRow';
+import Avatar from '@material-ui/core/Avatar';
+import { ReactComponent as VectorDown } from 'assets/img/Vectordown2.svg';
+import Button from '@material-ui/core/Button';
 
 import useStyles from './style';
 
@@ -19,6 +25,39 @@ const PaymentComponent = () => {
       <TableContainer className={classes.tableWrapper} component={Paper}>
         <Table>
           <TableHead rows={tableHeadArray} />
+          <TableBody>
+            <TableRow>
+
+              <TableCell className={classes.specialCell}>
+                <Avatar>vw</Avatar>
+                Apple Mac Book 15” 250 SSD 12GB
+              </TableCell>
+              <TableCell>
+                $73430
+              </TableCell>
+              <TableCell>
+                1234567890
+                {' '}
+              </TableCell>
+              <TableCell>
+                12:30
+                {' '}
+              </TableCell>
+              <TableCell>
+                <Button
+                  size="small"
+                  variant="outlined"
+                  className={classes.statusButton}
+                >
+                  <span className="circle" />
+                  Reconciled
+                </Button>
+              </TableCell>
+              <TableCell>
+                <VectorDown />
+              </TableCell>
+            </TableRow>
+          </TableBody>
         </Table>
       </TableContainer>
       <TableBottomBar />
--- src/components/Payment/style.js
+++ src/components/Payment/style.js
@@ -15,6 +15,31 @@ const useStyles = makeStyles((theme) => ({
     margin: '2rem 0rem',
     boxShadow: 'none',
   },
+  specialCell: {
+    display: 'flex',
+    alignItems: 'center',
+    '& .MuiAvatar-root': {
+      marginRight: '26px',
+      height: '36px',
+      width: '36px',
+      background: theme.palette.text.secondaryShade3,
+      fontWeight: 'normal',
+    },
+  },
+  statusButton: {
+    width: '120px',
+    textTransform: 'none',
+    borderRadius: '30px',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'space-around',
+    '& .circle': {
+      width: '9px',
+      height: '9px',
+      borderRadius: '50%',
+      background: 'red',
+    },
+  },
 }));
 
 export default useStyles;
