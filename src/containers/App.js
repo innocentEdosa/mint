f246f9be0486eda3ed55b3df8b2c321a5548e9c4
innocentEdosa <ilegbinijieinnocentcareer@gmail.com>
2020-03-30 10:04:27 +0100

    [104] create dashbaord layout

--- src/containers/App.js
+++ src/containers/App.js
@@ -3,14 +3,13 @@ import React from 'react';
 import theme from 'theme/theme';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import TopBar from 'components/TopBar';
-import SideNav from 'components/SideNav';
+import DashboardLayout from 'components/Layouts';
 
 const App = () => (
   <ThemeProvider theme={theme}>
     <CssBaseline />
     <TopBar />
-    <SideNav />
-    <div>this is the transMonitor application</div>
+    <DashboardLayout />
   </ThemeProvider>
 );
 
